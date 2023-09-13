9eb15f4b4dc424e98b1d2ee2cb655329b40a3f58
manel <manelbenali13@gmail.com>
2023-09-13 16:54:54 +0200

    modifs

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -2,52 +2,30 @@
 
 namespace App\Form;
 
-use Stripe\File;
 use App\Entity\Product;
-use App\Entity\Category;
+use Stripe\File;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, [
-                'label' => 'Nom du produit',
-                'attr' => ['placeholder' => 'Entrez le nom du produit']
-            ])
-            ->add('description', TextareaType::class, [
-                'label' => 'Description',
-                'attr' => ['rows' => 4]
-            ])
-            ->add('price', NumberType::class, [
-                'label' => 'Prix',
-                'scale' => 2
-            ])
-            ->add('CreatedAt', DateTimeType::class, [
-                'label' => 'Date et heure :',
-                'widget' => 'single_text',
-                'required' => true,
-                'attr' => ['autocomplete' => 'off'],
-                'input' => 'datetime_immutable'
-            ])
-            ->add('category', EntityType::class, [
-                'label' => 'Catégorie',
-                'class' => Category::class,
-                'placeholder' => 'Sélectionnez une catégorie'
-            ])
+            ->add('name')
+            ->add('description')
+            ->add('price')
+            ->add('category')
             ->add('imageFile', FileType::class, [
                 'label' => 'Image',
-                'attr' => ['class' => 'custom-file-input'],
+
+                // unmapped means that this field is not associated to any entity property
                 'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
                 'required' => false
             ])
         ;
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,12 +6,13 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
@@ -56,10 +57,9 @@ class RegistrationFormType extends AbstractType
             ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
-                'label' => 'Acceptez les conditions',
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'Vous devez acceptez les conditions.',
+                        'message' => 'You should agree to our terms.',
                     ]),
                 ],
             ])
@@ -67,13 +67,21 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'Nouveau mot de passe'],
+                'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
-                    new Regex('/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{14,}$/', "Il faut saisir un mot de passe de 14 caractères avec 1 majuscule, 1 minuscule, 1 chiffre et 1 caractère spécial")
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
                 ],
                 'label' => 'Mot de passe'
             ])
-            ;
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
