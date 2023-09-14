c500de9bf52114112c2482965daa552b069d3a78
manel <manelbenali13@gmail.com>
2023-09-14 11:57:14 +0200

    modifs

---
+++ migrations/Version20230913180831.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230913180831 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE contact (id INT AUTO_INCREMENT NOT NULL, lastname VARCHAR(100) NOT NULL, email VARCHAR(180) NOT NULL, phone VARCHAR(20) NOT NULL, message VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE category ADD created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE name name VARCHAR(50) NOT NULL');
+        $this->addSql('ALTER TABLE `order` ADD created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', DROP payment_id, CHANGE reference reference VARCHAR(50) NOT NULL, CHANGE paid_at paid_at DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE order_item ADD created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE order_item RENAME INDEX idx_52ea1f09558fbeb9 TO IDX_52EA1F098D9F6D38');
+        $this->addSql('ALTER TABLE product ADD created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', CHANGE name name VARCHAR(50) NOT NULL');
+        $this->addSql('ALTER TABLE user ADD created_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE contact');
+        $this->addSql('ALTER TABLE category DROP created_at, CHANGE name name VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE `order` ADD payment_id VARCHAR(50) NOT NULL, DROP created_at, CHANGE reference reference VARCHAR(255) NOT NULL, CHANGE paid_at paid_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE order_item DROP created_at');
+        $this->addSql('ALTER TABLE order_item RENAME INDEX idx_52ea1f098d9f6d38 TO IDX_52EA1F09558FBEB9');
+        $this->addSql('ALTER TABLE user DROP created_at');
+        $this->addSql('ALTER TABLE product DROP created_at, CHANGE name name VARCHAR(255) NOT NULL');
+    }
+}
